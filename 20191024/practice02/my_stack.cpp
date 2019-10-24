f28ad1cab3508b07931bbba20d858248ac7a87d2
cbchoi <cbchoi@handong.edu>
2019-10-24 22:22:45 +0900

    solution

--- 20191024/practice02/my_stack.cpp
+++ 20191024/practice02/my_stack.cpp
@@ -3,41 +3,79 @@
 // Creation
 Stack* CreateStack(int max_stack_size)
 {
-    
+    /* dynamically allocate data structure */
+    Stack *s = new Stack; 
+    /* dynamically allocate elements */
+    s->stack = new Element[max_stack_size];
+    s->max_size = max_stack_size;
+    /* Initially top points to -1 */
+    s->top = -1;
+    return s;
+
 }
 
 // Transform
 Stack* DuplicateStack(Stack* original)
 {
-    
+    Stack *s = CreateStack(original->max_size); /* Create stack */
+    s->max_size = original->max_size;
+    s->top = original->top;
+    for(int i = 0; i < s->top+1; i++)
+         s->stack[i] = original->stack[i];                               /* Clone the elements */
+    return s;
+
 }
 
 // Report
 bool IsFullStack(Stack *s)
 {
-   
+   if(s->top == s->max_size - 1) 
+        return 1;
+   else 
+        return 0;
+
 }
 
 bool IsEmptyStack(Stack *s)
 {
-
+   if(s->top < 0) 
+        return 1;
+   else 
+        return 0;
 }
 
 // Operation on Stack
 void Push(Stack *s, Element item)
 {
+   if(IsFullStack(s)) 
+     /* prints an error message */
+      std::cout << "Stack Full" << std::endl;	
+   else {
+      s->stack[++s->top] = item;
+   }
 
 }
 
 Element Pop(Stack *s)
 {
+   if(IsEmptyStack(s)) 
+   {
+   	/* prints an error message */
+   	  std::cout << "Stack Empty" << std::endl;
+   	  return Element();	
+   }   
+   else {
+      return s->stack[s->top--];
+   }
 
 }
 
 // Destruction
 void DestroyStack(Stack *s)
 {
-
+     delete [] s->stack;
+     delete s;
+     s = nullptr;
 }
 
 // Utility
--- 20191024/practice02/stack_app.cpp
+++ 20191024/practice02/stack_app.cpp
@@ -6,6 +6,38 @@
 
 int main()
 {
-	
+	char buf[100];
+	Stack* s = CreateStack(100);
+
+	std::cout << "Enter expression:";
+	std::cin.getline(buf, 100);
+
+	for(unsigned int i = 0; i < strlen(buf);i++)
+	{
+		if(buf[i] == '(' || buf[i] == ')')
+		{
+			if(buf[i] == '(')
+			{
+				Element e;
+				e.ch = buf[i];
+				Push(s, e);
+			}	
+			else if(buf[i] == ')')
+			{
+				if(IsEmptyStack(s))
+				{
+					std::cout << "unpaired" << std::endl;
+					exit(0);
+				}
+				Pop(s);
+			}
+		}
+	}
+
+	if(IsEmptyStack(s))
+		std::cout << "paired" << std::endl;
+	else
+		std::cout << "unpaired" << std::endl;
+
 	return 0;
 }
